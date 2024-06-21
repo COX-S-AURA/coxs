713a64071b3bae4e1c0f7ce413671c327bb08ecf
MJ AHMAD <168907070+mjahmad1@users.noreply.github.com>
2024-06-21 15:59:50 -0700

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "mjahmad";
+$username = "MJ-AHMAD";
+$password = "@New2024#";
+$dbname = "INNO";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert data
+$sql = "INSERT INTO users (name, email) VALUES ('".$_POST['name']."', '".$_POST['email']."')";
+
+if ($conn->query($sql) === TRUE) {
+echo "New record created successfully";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
